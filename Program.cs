c17e544291971bbc780978feaa1528c14d855989
Triliam <taisbuenovidotto@gmail.com>
2025-08-22 18:08:34 -0300

    Refactored code with REST API best practices

--- Program.cs
+++ Program.cs
@@ -4,62 +4,42 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//endpoint - rotas
-//get post
-app.MapGet("/", () => "Hello World!");
-
-app.MapPost("/", () => new { Name = "Taís", Age = 40 });
-
-app.MapGet("/AddHeader", (HttpResponse response) =>
-{
-    response.Headers.Add("Testando", "Taís");
-    return new { Name = "Talita", Age = 40 };
-});
-
-//parametro por URL
-//api.app.com/users?datastart={date}&dataend={date}
-app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>
-{
-    return dateStart + " - " + dateEnd;
-});
-
 
 //parametro pelo body
-app.MapPost("/saveproduct", (Product product) =>
+app.MapPost("/products", (Product product) =>
 {
     //CRUD - C
     ProductRepository.Add(product);
+    return Results.Created("/products", product.Code);
 });
 
 //api.app.com/user/{code}
-app.MapGet("/getproduct/{code}", ([FromRoute]string code) =>
+app.MapGet("/products/{code}", ([FromRoute]string code) =>
 {
     //CRUD - R
     var product = ProductRepository.GetByCode(code);
-    return product;
+    if (product != null)
+    {
+        return Results.Ok(product);
+    }
+    return Results.NotFound();
   
 });
 
-app.MapPut("/editproduct", (Product product) =>
+app.MapPut("/products", (Product product) =>
 {
     //CRUD - U
     var productSaved = ProductRepository.GetByCode(product.Code);
     productSaved.Name = product.Name;
+    return Results.Ok();
 });
 
-app.MapDelete("/deleteproduct/{code}", ([FromRoute] string code) =>
+app.MapDelete("/products/{code}", ([FromRoute] string code) =>
 {
     //CRUD - D
     var product = ProductRepository.GetByCode(code);
     ProductRepository.Remove(product);
-
-});
-
-
-//parametro pelo header
-app.MapGet("/getproductbyheader", (HttpRequest request) =>
-{
-    return request.Headers["product-code"].ToString;
+    return Results.Ok();
 });
 
 
