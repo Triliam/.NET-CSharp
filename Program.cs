7035c2432c66fd21de4fcb907da8740d80d3e56e
Triliam <taisbuenovidotto@gmail.com>
2025-08-24 10:31:09 -0300

    fluent api

--- Program.cs
+++ Program.cs
@@ -65,6 +65,13 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
+
+        modelBuilder.Entity<Product>().Property(p => p.Code).HasMaxLength(20).IsRequired();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@Sql2019;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
 
 }
